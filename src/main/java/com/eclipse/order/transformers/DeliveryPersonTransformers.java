4c3e59defb90928c82e8a521c5bee5c63ae78457
WAIL BENDAOUD <bendaoudwail@gmail.com>
2023-11-30 00:40:37 +0100

    transformers_WBE

---
+++ src/main/java/com/eclipse/order/transformers/DeliveryPersonTransformers.java
@@ -0,0 +1,43 @@
+package com.eclipse.order.transformers;
+
+import com.eclipse.order.dtos.DeliveryPersonDto;
+import com.eclipse.order.entities.DeliveryPerson;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DeliveryPersonTransformers {
+        public static DeliveryPerson toEntity(DeliveryPersonDto dto) {
+            DeliveryPerson entity = DeliveryPerson.builder()
+                    .id(dto.getId())
+                    .fullname(dto.getFullname())
+                    .status(dto.getStatus())
+                    .city(dto.getCity())
+                    .email(dto.getEmail())
+                    .phone(dto.getPhone())
+                    .build();
+            return entity;
+        }
+
+
+
+        public static DeliveryPersonDto toDto(DeliveryPerson entity) {
+            DeliveryPersonDto dto = DeliveryPersonDto.builder()
+                    .id(entity.getId())
+                    .fullname(entity.getFullname())
+                    .status(entity.getStatus())
+                    .city(entity.getCity())
+                    .email(entity.getEmail())
+                    .phone(entity.getPhone())
+                    .build();
+            return dto;
+        }
+
+
+        public static List<DeliveryPersonDto> toDtoList(List<DeliveryPerson> entities) {
+            return entities.stream().map(entity -> toDto(entity)).collect(Collectors.toList());
+        }
+
+
+    
+}
---
+++ src/main/java/com/eclipse/order/transformers/OrderTransformer.java
@@ -0,0 +1,40 @@
+package com.eclipse.order.transformers;
+
+import com.eclipse.order.dtos.OrderDto;
+import com.eclipse.order.entities.Order;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderTransformer {
+
+    public static Order toEntity(OrderDto dto) {
+        Order entity = Order.builder()
+            .id(dto.getId())
+            .products(dto.getCommandItems())
+            .status(dto.getStatus())
+            .customer(dto.getClient())
+            .totalPrice(dto.getTotalPaye())
+            .build();
+
+            return entity;
+    }
+
+    public static OrderDto toDto(Order entity) {
+        OrderDto dto = OrderDto.builder()
+            .id(entity.getId())
+            .commandItems(entity.getProducts())
+            .client(entity.getCustomer())
+            .deliveryPerson(entity.getDeliveryPerson() != null ? DeliveryPersonTransformers.toDto(entity.getDeliveryPerson()) : null)
+            .status(entity.getStatus())
+            .totalPaye(entity.getTotalPrice())
+            .build();
+
+        return dto;
+    }
+
+    public static List<OrderDto> toDtoList(List<Order> entities) {
+        return entities.stream().map(entity -> toDto(entity)).collect(Collectors.toList());
+    }
+    
+}
